4304de917124e39f11233a99d79a5f05a060f7d7
Isac <isac4322@gmail.com>
2020-12-28 16:01:31 -0300

    complet mummy moviment

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,9 +7,9 @@ public class Enemy : MonoBehaviour
     // Private variables
 
     //public LootGenerator LootGen;
-    [SerializeField] int lives;
-    [SerializeField] float movimentSpeed;
-    [SerializeField] int powerAttack;
+    [SerializeField] protected int lives;
+    [SerializeField] protected float movimentSpeed;
+    [SerializeField] protected int powerAttack;
 
     // Private Functions
     void Die() 
--- Assets/Scripts/Enemy/MummyController.cs
+++ Assets/Scripts/Enemy/MummyController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class MummyController : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    // Public Variables
+    public bool isMoving;
+
+    // Private Variables
+    [SerializeField] Transform minPos, maxPos;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Transform mummyTransform;
+    int direction = 1; // -1 to move left and 1 to move right
+
+    private void FixedUpdate()
     {
-        
+        if (isMoving)
+        {
+            Move(direction);
+
+            if (mummyTransform.position.x >= maxPos.position.x)
+            {      
+           
+                if (rb.velocity.x > 0) // is moving to right
+                {                
+                    direction = -1; // turn to left
+                    mummyTransform.localScale = new Vector2(mummyTransform.localScale.x * -1,mummyTransform.localScale.y);
+                }
+
+            }
+            else if (mummyTransform.position.x <= minPos.position.x) 
+            {
+                if (rb.velocity.x < 0) // is moving to left
+                {             
+                    direction = 1; // turn to right
+                    mummyTransform.localScale = new Vector2(mummyTransform.localScale.x * -1, mummyTransform.localScale.y);
+                }
+
+            }
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    // Private Functios
+    void Move(int direction) // -1 to move left and 1 to move right
     {
-        
+        rb.velocity = new Vector2(Time.deltaTime * movimentSpeed * direction, rb.velocity.y);
     }
 }
