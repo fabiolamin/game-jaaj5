6666e7d2ca96760f639cb1f17b2d4593e8ad388c
Fábio Lamin <fabio_lamin@hotmail.com>
2021-01-02 11:29:32 -0300

    Implement breakable grounds

---
+++ Assets/Scripts/Ground/BreakableGround.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class BreakableGround : MonoBehaviour
+{
+    [SerializeField] private float delayToBreak = 0.1f;
+    [SerializeField] private GameObject breakableGroundParticles;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        CheckIfPlayerCanBreak();
+    }
+
+    private void CheckIfPlayerCanBreak()
+    {
+        if (PlayerManager.Instance.PlayerAttack.CanAttackByAir())
+        {
+            StartCoroutine(AwaitToBreak());
+        }
+        else
+        {
+            PlayerManager.Instance.PlayerAttack.IsReadyToAttackByAir = false;
+        }
+    }
+
+    private IEnumerator AwaitToBreak()
+    {
+        yield return new WaitForSeconds(delayToBreak);
+        Break();
+    }
+
+    private void Break()
+    {
+        PlayerManager.Instance.PlayerAttack.IsReadyToAttackByAir = false;
+        Instantiate(breakableGroundParticles, transform.position, Quaternion.identity);
+        gameObject.SetActive(false);
+    }
+}
